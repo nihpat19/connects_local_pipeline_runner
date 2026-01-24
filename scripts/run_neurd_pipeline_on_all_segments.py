2c03535d6c50144233a18cc75f380aae13f7d5c2
nihpat19 <nihpat77@gmail.com>
2026-01-23 21:14:10 -0600

    Add code to run on all segments

--- scripts/run_neurd_pipeline_on_all_segments.py
+++ scripts/run_neurd_pipeline_on_all_segments.py
@@ -2,12 +2,13 @@ import datajoint as dj
 import os
 import sys
 #sys.path.append('../../connects_local_pipeline_runner')
+import numpy as np
 
 import run_neurd_pipeline
 m35d = dj.create_virtual_module('minnie35_download','nihil_minnie35_download')
 m35p = dj.create_virtual_module('minnie35_process', 'nihil_minnie35_process')
 
-segments_to_proofread = (m35d.Segment - m35p.AutoProofreadNeuron).fetch('segment_id').tolist()
-
-run_neurd_pipeline.run_segments(segments_to_proofread,delete_existing_jobs=True)
+segments_to_proofread_splits = np.array_split((m35d.Segment - m35p.AutoProofreadNeuron).fetch('segment_id'), 300)
+for segments in segments_to_proofread_splits:
+    run_neurd_pipeline.run_segments(segments.tolist(),delete_existing_jobs=True)
 
