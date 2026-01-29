50c0382c10e69323fc0485e377c2a7cd3ea33278
nihpat19 <nihpat77@gmail.com>
2026-01-28 19:29:23 -0600

    Limit number of populate calls to ease pressure on database server

--- scripts/run_neurd_pipeline_test.py
+++ scripts/run_neurd_pipeline_test.py
@@ -1,15 +1,14 @@
 import datajoint as dj
-import os
-import sys
-#sys.path.append('../../connects_local_pipeline_runner')
-import numpy as np
 
+import numpy as np
 import run_neurd_pipeline
+import math
+
 m35d = dj.create_virtual_module('minnie35_download','nihil_minnie35_download')
 m35p = dj.create_virtual_module('minnie35_process', 'nihil_minnie35_process')
+segments_to_proofread = ((m35d.DecimatedMesh * m35d.SomaInfo - m35p.AutoProofreadNeuron) & 'n_somas=1').fetch('segment_id')
+batch_size = 1000
+num_batches = math.ceil(len(segments_to_proofread)/batch_size)
+segments_to_proofread_splits = np.array_split(segments_to_proofread,num_batches)
 
-segments_to_proofread = ((m35d.DecimatedMesh * m35d.SomaInfo)
-                         - m35p.AutoProofreadNeuron).fetch('segment_id',order_by='n_somas')[:10]
-
-run_neurd_pipeline.run_segments(segments_to_proofread,delete_existing_jobs=True)
-
+run_neurd_pipeline.run_segments(segments_to_proofread_splits[0].tolist(),delete_existing_jobs=True)
