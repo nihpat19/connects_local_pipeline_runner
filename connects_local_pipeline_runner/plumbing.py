9c49456730a2e7257338ff3932546f40907fe6f5
nihpat19 <nihpat77@gmail.com>
2026-03-12 16:04:27 -0500

    Only use xlarge resource group

--- connects_local_pipeline_runner/plumbing.py
+++ connects_local_pipeline_runner/plumbing.py
@@ -4,8 +4,8 @@ from datajoint.hash import key_hash as kh
 from connects_local_pipeline_runner import abstracted, monitoring, clusters
 import sys
 sys.path.append('/home/nihil/Documents/connects_m65_proofread')
-import minnie65download
-import minnie65process
+#import minnie65download
+#import minnie65process
 schema = dj.Schema("nihil_m65plumbing")
 
 from getpass import getuser
@@ -91,12 +91,12 @@ class ResourceModel(dj.Lookup):
        if model == 'neurd-soma-low':
            return 'r6g.large' if table == 'SomaExtraction' else 'r6g.xlarge'
        if model == 'neurd':
-            key_segment = (Keys() & f'key_hash="{key_hash}"').key[0]['segment_id']
-            segment_filesize_in_mb = (minnie65download.schema.external['decimated_meshes'] & f'filepath like "%{key_segment}%"').fetch1('size')/1e6
-            if segment_filesize_in_mb>10:
-                return 'r6g.xlarge'
-            else:
-                return 'r6g.large'
+            # key_segment = (Keys() & f'key_hash="{key_hash}"').key[0]['segment_id']
+            # segment_filesize_in_mb = (minnie65download.schema.external['decimated_meshes'] & f'filepath like "%{key_segment}%"').fetch1('size')/1e6
+            # if segment_filesize_in_mb>10:
+            return 'r6g.xlarge'
+            # else:
+            #     return 'r6g.large'
 
 
 
