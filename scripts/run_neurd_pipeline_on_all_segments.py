650020ee1c884ae018c16968e895b44d42f19f8c
nihpat19 <nihpat77@gmail.com>
2026-03-16 14:48:25 -0500

    Add CAVECLIENT token environment variable and MeshDecimation table to plumbing and abstract schemas

--- scripts/run_neurd_pipeline_on_all_segments.py
+++ scripts/run_neurd_pipeline_on_all_segments.py
@@ -5,10 +5,10 @@ import sys
 import numpy as np
 
 import run_neurd_pipeline
-m35d = dj.create_virtual_module('minnie35_download','nihil_minnie35_download')
+v1d = dj.create_virtual_module('v1dd_download','nihil_v1dd_download')
 v1p = dj.create_virtual_module('v1dd_process', 'nihil_v1dd_process')
 
-segments_to_proofread_splits = (v1p.MeshDecimation - v1p.AutoProofreadNeuron).fetch('segment_id')
-for segments in segments_to_proofread_splits:
-    run_neurd_pipeline.run_segments(segments.tolist(),delete_existing_jobs=True)
+segments_to_proofread_splits = (v1d.DownloadedMesh - v1p.MeshDecimation).fetch('segment_id')
+#for segments in segments_to_proofread_splits:
+run_neurd_pipeline.run_segments(segments_to_proofread_splits.tolist(),delete_existing_jobs=True)
 
