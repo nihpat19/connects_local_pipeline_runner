26a0c446f5f75783dfefd59c2d0c9675317f7cb2
nihpat19 <nihpat77@gmail.com>
2026-07-31 09:03:46 -0500

    Fix soma extraction conditions

--- connects_local_pipeline_runner/plumbing.py
+++ connects_local_pipeline_runner/plumbing.py
@@ -98,10 +98,10 @@ class ResourceModel(dj.Lookup):
        if model == 'neurd':
             key_segment = (Keys() & f'key_hash="{key_hash}"').key[0]['segment_id']
             segment_filesize_in_mb = (diamonddownload.schema.external['decimated_meshes'] & f'filepath like "{key_segment}%"').fetch1('size') / 1e6
-            if (len(diamonddownload.DecimatedMesh & f'segment_id={key_segment}')>0) and (segment_filesize_in_mb>200) and ((diamondprocess.SomaExtraction & f'segment_id={key_segment}').fetch1('n_somata')<=10):
-                return 'r6g.xxlarge'
-            elif (len(diamondprocess.SomaExtraction & f'segment_id={key_segment}')>0):
-                  if (((diamondprocess.SomaExtraction & f'segment_id={key_segment}').fetch1('n_somata')>10) or ((diamondprocess.SomaExtraction & f'segment_id={key_segment}').fetch1('runtime')>1000)):
+            if (len(diamonddownload.DecimatedMesh & f'segment_id={key_segment}')>0) and (len(diamondprocess.SomaExtraction & f'segment_id={key_segment}')>0): 
+                if (segment_filesize_in_mb>200) and ((diamondprocess.SomaExtraction & f'segment_id={key_segment}').fetch1('n_somata')<=10):
+                    return 'r6g.xxlarge'
+                elif (((diamondprocess.SomaExtraction & f'segment_id={key_segment}').fetch1('n_somata')>10) or ((diamondprocess.SomaExtraction & f'segment_id={key_segment}').fetch1('runtime')>1000)):
                     return 'r6g.3xlarge'
             else:
                 if segment_filesize_in_mb>800:
